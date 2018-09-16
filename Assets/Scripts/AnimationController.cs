b272246d451bc911e60929fe0617ad6f00a9227f
qw893821 <xiuman893821@gmail.com>
2018-09-16 01:18:01 -0400

    update
    1.adjust model from asset store for my project
    2. add a interface to this project

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+    public Animator anim;
+    public bool walk, run, idle;
+
+    void AnimeSelect()
+    {
+        if (walk)
+        {
+            anim.SetBool("Walk", true);
+            anim.SetBool("Run",false);
+        }
+        else if (run)
+        {
+            anim.SetBool("Walk",false);
+            anim.SetBool("Run", true);
+        }
+    }
+
+}
