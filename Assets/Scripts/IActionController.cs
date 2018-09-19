14b88879dca3a9ae0e6a7ebfa739b96d8ef56f9b
qw893821 <xiuman893821@gmail.com>
2018-09-18 21:02:24 -0400

    update
    add navmeshagent

--- Assets/Scripts/IActionController.cs
+++ Assets/Scripts/IActionController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IActionController  {
-
-	
+public interface IActionController {
+    float speed { get; set; }
+    
+    void Move();
+    void Attack();
 }
---
+++ Assets/Scripts/PlayerActionController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerActionController: MonoBehaviour, IActionController {
+    Vector3 targetPos;
+    public float speed { get; set; }
+
+    NavMeshAgent agent;
+    void Start()
+    {
+        agent = transform.GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        Move();
+    }
+    public virtual void Move()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            agent.destination = TargetPos();
+            Debug.Log("move");
+        }
+        //agent.destination = targetPos;
+    }
+
+    
+
+    public void Attack()
+    {
+
+    }
+
+    Vector3 TargetPos (){
+        Vector3 pos;
+
+        pos = Vector3.zero;
+        return pos;
+    }
+}
