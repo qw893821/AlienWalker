b19b1c6a524befe2025b415d33c200a27c0434b9
qw893821 <xiuman893821@gmail.com>
2018-09-29 00:47:05 -0400

    update
    add "view" to zombie monster

---
+++ Assets/Scripts/NewMonsterView.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewMonsterView : MonoBehaviour {
+    [Range(30.0f,90.0f)]
+	public float viewAngle;
+    public float viewRange;
+
+    private void Update()
+    {
+        TargetInViewCheck();
+    }
+
+    void TargetInViewCheck()
+    {
+        Collider [] inrangeCol;
+        LayerMask selfLayer=1<<8;
+        Vector3 currentPos;
+        currentPos = transform.position;
+        inrangeCol=Physics.OverlapSphere(currentPos, viewRange);
+       
+        foreach(Collider col in inrangeCol)
+        {
+            if (col.transform.position != transform.position)
+            {
+                Vector3 colPos;
+                colPos = col.transform.position;
+                if (Vector3.Angle(transform.forward, colPos - currentPos) < viewAngle / 2)
+                {
+                    RayToTarget(col.transform);
+                }
+            }
+        }
+        
+    }
+
+
+    void RayToTarget(Transform trans)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, trans.position,out hit, viewRange))
+        {
+            if (hit.transform.tag == "Player")
+            {
+                Debug.Log("player in view");
+            }
+        }
+
+    }
+}
